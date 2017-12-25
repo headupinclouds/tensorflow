27a467ea9ae12dcaf3b3bed96ef7dbaed3ddb601
David Hirvonen <dhirvonen@elucideye.com>
2017-12-25 02:09:27 -0500

    wip build scripts

---
+++ tensorflow/contrib/cmake/bin/build-libcxx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# tensorflow_ENABLE_GPU "Enable GPU support" OFF)
+# tensorflow_BUILD_CC_TESTS)
+
+tf_opts=(
+    CUDA_TOOLKIT_ROOT_DIR=/Developer/NVIDIA/CUDA-9.0
+    CUDA_TOOLKIT_TARGET_DIR=/Developer/NVIDIA/CUDA-9.0
+    tensorflow_PATH_STATIC_LIB=/Developer/NVIDIA/CUDA-9.0/lib
+    tensorflow_CUDNN_INCLUDE=$(dirname $(dirname ${CUDNN_PATH}))/include
+    tensorflow_PATH_CUDNN_STATIC_LIB=$(dirname ${CUDNN_PATH})
+    tensorflow_ENABLE_GPU=ON
+    tensorflow_BUILD_CC_TESTS=ON
+    tensorflow_ENABLE_GRPC_SUPPORT=ON # needs 1.8
+)
+
+#polly.py --toolchain xcode-hid-sections --fwd ${tf_opts[@]} --reconfig --verbose ${*}
+polly.py --toolchain libcxx --config Release --fwd ${tf_opts[@]} --verbose ${*} --keep-going --jobs 8
